8f10d8392418a8106285c4e47b215e64f10eaae6
gauravdattjoshi <letslearncbse0@gmail.com>
2020-10-08 17:34:14 +0530

    auth token fixed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,32 @@
-const jwt=require('jsonwebtoken')
-const config=require('../config/config.json')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config.json')
 
-function authenticateToken(req, res, next) {
-    // Gather the jwt access token from the request header
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401) // if there isn't any token
-  
-    jwt.verify(token, config.secret, (err,  user) => {
-      console.log(err)
-      if (err) return res.sendStatus(403)
-      req.user = user
-      next() // pass the execution off to whatever request the client intended
-    })
-  };
-  module.exports={
-      authenticateToken:authenticateToken};
+async function authenticateToken(req, res, next) {
+  const TOKEN_VALIDATION_MSG = "Invalid token or token is not present";
+
+  const authHeader = req.headers.authorization;
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).send({
+      success: false,
+      message: TOKEN_VALIDATION_MSG
+    });
+  }
+
+
+  try {
+    const payload=await jwt.verify(token, config.secret);
+    req.payload = payload;
+    console.log(" token verified");
+    next();
+  } catch (error) {
+    return res.status(403).send({
+      success: false,
+      message: TOKEN_VALIDATION_MSG
+    });
+  }
+
+}
+module.exports =  authenticateToken;
