5a009be5c58aa08c75bfef0c59b283cefd897ca9
jayagupta12 <gupta.jaya3@gmail.com>
2020-10-09 13:59:04 +0530

    swagger

--- src/index.js
+++ src/index.js
@@ -5,17 +5,28 @@ const dbCon = require("./database/connection")
 const bodyParser = require("body-parser")
 let authMiddleware = require('./middleware/auth')
 const unless = require('express-unless');
-
+const swaggerUi=require('swagger-ui-express')
+const swaggerJSDoc=require('swagger-jsdoc')
 const app = express();
 
 authMiddleware.unless = unless;
 app.use(
   authMiddleware.unless({
-    path: [/\/api\/v1\/auth*/],
+    path: [/\/api\/v1\/auth*/,/\/api-documentation/],
   })
 );
 
-
+const options={
+  definition:{
+    openApi:'6.14.8',
+    info:{
+      title:'Swagger Express API',
+      version:'1.0.0'
+    }
+  },
+  apis:[user]
+};
+const swaggerSpec=swaggerJSDoc(options);
 
 
 app.use(bodyParser.json());
@@ -24,3 +35,4 @@ dbCon.connectToDB();
 app.listen(config.port, () => console.log(`server started at port ${config.port}`))
 
 app.use("/api/v1/auth", user)
+app.use('/api-documentation',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
--- src/router/user_route.js
+++ src/router/user_route.js
@@ -3,6 +3,26 @@ const express=require('express');
 const router=express.Router();
 const user=require('../controllers/user')
 
+
+/**
+ * @swagger
+ * 
+ * /auth/createUser
+ *   post:
+ *     summary:register user
+ *     description:register a new user
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               name:
+ *                 type:string
+ *               email:
+ *                 type:string
+ *               password:
+ *                 type:string
+ */
 router.post('/createUser',user.CreateUser)
 router.post('/loginUser',user.LoginUser)
 module.exports=router;
