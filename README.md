a4d7df32ec71d89f9591648f866104778942e326
gauravdattjoshi <letslearncbse0@gmail.com>
2020-10-09 19:48:56 +0530

    swagger issues fixed

---
+++ README.md
@@ -0,0 +1,4 @@
+### swagger specifications
+https://swagger.io/docs/specification/2-0/
+### swagger js docs - github
+https://github.com/Surnet/swagger-jsdoc/blob/master/docs/GETTING-STARTED.md
--- src/router/user_route.js
+++ src/router/user_route.js
@@ -1,17 +1,18 @@
-const express=require('express');
+const express = require('express');
 
-const router=express.Router();
-const user=require('../controllers/user')
+const router = express.Router();
+const user = require('../controllers/user')
 
 
 /**
  * @swagger
- * 
- * /auth/createUser
- *   post:
- *     summary:register user
- *     description:register a new user
+ * path:
+ *  /auth/createUser:
+ *    post:
+ *     summary: register user
+ *     description: register a new user
  *     requestBody:
+ *       required: true
  *       content:
  *         application/json:
  *           schema:
@@ -22,7 +23,49 @@ const user=require('../controllers/user')
  *                 type:string
  *               password:
  *                 type:string
+ *     responses:
+ *        "200":
+ *          description: A user created
+ *          content:
+ *            application/json:
+ *             schema:
+ *              properties:
+ *               name:
+ *                 type:string
+ *               email:
+ *                 type:string
+ *               password:
+ *                 type:string
+ *              
  */
-router.post('/createUser',user.CreateUser)
-router.post('/loginUser',user.LoginUser)
-module.exports=router;
+router.post('/createUser', user.CreateUser)
+
+
+/**
+ * @swagger
+ *
+ * /auth/loginUser:
+ *   post:
+ *     description: login user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: Request body
+ *         description: request body for login
+ *         in:  body
+ *         required: true
+ *         schema: 
+ *           type: object
+ *           properties:
+ *             email:
+ *                type: string
+ *             password:
+ *                 type: string
+ *              
+ *     responses:
+ *       200:
+ *         description: users
+ *              
+ */
+router.post('/loginUser', user.LoginUser)
+module.exports = router;
