a52c632866eaf6da482fe27573f6327d2addda51
jayagupta12 <gupta.jaya3@gmail.com>
2020-10-18 15:10:12 +0530

    blog upload

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -3,14 +3,25 @@ const dbService = require('../services/database_service')
 
 
 
-exports.CreateBlog = async (req, res, next) => {
+exports.CreateBlog = async (req, res) => {
 
-        let response = await dbService.add("blog", req.body);
-    try {
-            return res.status(201).json({
-                    message: 'File uploded successfully'
-            });
-    } catch (error) {
-            console.error(error);
-    }
+        console.log("file==>", req.file);// middle ware will upload ur files and add the file info in req.file/req.files
+        console.log("body==>", req.body); // and form data will be present inside req.body
+
+
+        let blogData = {
+                title: req.body.title,
+                description: req.body.description,
+                postedby: req.body.postedby,
+                postdate: req.body.postdate,
+                imagePath: req.file.path,
+                image: req.file.filename
+        }
+        try {
+                let response = await dbService.add('blog', blogData)
+                res.send(response);
+
+        } catch (error) {
+                console.error(error);
+        }
 }
